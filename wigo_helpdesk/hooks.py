1bdc731749a075b4c1cec18cc3b5f739fe2a499a
Jhonny Rojas Flores <jhonnyrojasflo@gmail.com>
2026-04-16 00:10:17 -0400

    feat: Enhance customer contract view with Cobranza tab and payment details

--- wigo_helpdesk/hooks.py
+++ wigo_helpdesk/hooks.py
@@ -2,8 +2,23 @@
 from odoo import api, SUPERUSER_ID
 
 
-def post_init_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def post_init_hook(env_or_cr, registry=None):
+    """Compat post-init hook.
+
+    Odoo versions differ on the hook signature:
+    - Newer versions call: post_init_hook(env)
+    - Older versions call: post_init_hook(cr, registry)
+
+    This implementation supports both.
+    """
+    if registry is None and hasattr(env_or_cr, 'cr'):
+        # Called as post_init_hook(env)
+        env = api.Environment(env_or_cr.cr, SUPERUSER_ID, dict(getattr(env_or_cr, 'context', {}) or {}))
+    else:
+        # Called as post_init_hook(cr, registry)
+        cr = env_or_cr
+        env = api.Environment(cr, SUPERUSER_ID, {})
+
     escalated_stage = env.ref('wigo_helpdesk.stage_escalated', raise_if_not_found=False)
     waiting_stage = env['helpdesk.stage'].search([('name', '=', 'En Espera')], limit=1)
 
