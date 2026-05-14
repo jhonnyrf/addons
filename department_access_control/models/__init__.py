16c9431b72f8e2d73880ab094f67460353e5a40e
jhonnyDL <jhonnydelgadillo003@gmail.com>
2026-05-14 12:53:45 -0400

    Fix: eliminacion de archivos inncesarios

--- department_access_control/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import department_access_rule
-from . import department_module_access
-from . import department_employee_module_access
-from . import hr_department
-from . import hr_employee
-from . import ir_ui_menu
-from . import ir_model_access
